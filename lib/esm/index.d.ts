f9b9ec361fdc78b4b471ca2bac0c354df40287a1
markgregg <gregg.mark@gmail.com>
2023-05-03 19:29:36 +0100

    removed lib

--- lib/esm/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const registerUmami: (url: string, websiteId: string) => void;
-export type Track = (name: string, data?: any) => void;
-export declare const useUmami2: (disabledPageView?: boolean, disableAll?: boolean) => Track;
-declare const useUmami: (url?: string, title?: string, hostname?: string, referrer?: string, width?: number, height?: number, disabledPageView?: boolean, disableAll?: boolean) => Track;
-export default useUmami;
