9df33da8a997c5c0b8c88071000cf32d4ad8d520
markgregg <gregg.mark@gmail.com>
2023-05-12 08:20:08 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,31 @@
-import { resolve } from 'node:path'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import EsLint from 'vite-plugin-linter'
-import tsConfigPaths from 'vite-tsconfig-paths'
-const { EsLinter, linterPlugin } = EsLint
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
-export default defineConfig((configEnv) => ({
-  plugins: [
-    react(),
-    tsConfigPaths(),
-    linterPlugin({
-      include: ['./src}/**/*.{ts,tsx}'],
-      linters: [new EsLinter({ configEnv })],
-    }),
-    dts({
-      insertTypesEntry: true,
-    })
-  ],
-  build: {
-    lib: {
-      entry: resolve('src', 'index.ts'),
-      name: 'pwa-synergy-umami',
-      formats: ['es', 'umd'],
-      fileName: (format) => `pwa-synergy-umami.${format}.js`,
+export default defineConfig({
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'pwa-synergy-umami',
+            formats: ['es', 'umd'],
+            fileName: (format) => `pwa-synergy-umami.${format}.js`,
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM'
+                },
+            },
+        },
     },
-    rollupOptions: {
-    },
-  },
-}))
+});
